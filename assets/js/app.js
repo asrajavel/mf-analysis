b6b21c18a6099d77e7e3012723f9f3ff8f5da1ee
rajavel.as <rajavel.as@flipkart.com>
2023-10-31 14:17:16 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,93 @@
+mutualFundsList = []
+
+
+$(function () {
+    searchText = null;
+    $("#mfsearch").autocomplete({
+        search: function (event, ui) {
+            searchText = encodeURIComponent(document.getElementById("mfsearch").value);
+        },
+        source: function (req, res) {
+            $.ajax({
+                dataType: "json",
+                type: 'GET',
+                url: 'https://api.mfapi.in/mf/search?q=' + searchText,
+                success: function (data) {
+                    $('#mfsearch').removeClass('ui-autocomplete-loading');
+                    // hide loading image
+                    res(data);
+                },
+                error: function (data) {
+                    $('#mfsearch').removeClass('ui-autocomplete-loading');
+                }
+            });
+
+        },
+        select: function (event, ui) {
+            // document.getElementById("mfsearch").value = " " + ui.item.schemeName;
+
+            // const div = document.createElement('div');
+            // div.className = 'row';
+            // div.innerHTML = `
+            //     <div schemeCode="${ui.item.schemeCode}" schemeName="${ui.item.schemeName}">${ui.item.schemeName}</div>
+            // `;
+            // document.getElementById('selectedMutualFunds').appendChild(div);
+
+            var selectedMutualFundsDiv = document.getElementById("selectedMutualFunds");
+    var schemeCode = ui.item.schemeCode;
+    var schemeName = ui.item.schemeName;
+
+    // Create the div with the close button and add it to the selectedMutualFundsDiv
+    var mutualFundDiv = createMutualFundDiv(schemeCode, schemeName);
+    selectedMutualFundsDiv.appendChild(mutualFundDiv);
+
+            main()
+        }
+
+    })
+        .data("ui-autocomplete")._renderItem = function (ul, item) {
+            return $("<li>")
+                .data("ui-autocomplete-item", item)
+                .append(item.schemeName)
+                .attr("schemeCode", item.schemeCode)
+                .appendTo(ul);
+        }
+});
+
+
+// Function to create a new div with a close button
+function createMutualFundDiv(schemeCode, schemeName) {
+    var div = document.createElement('div');
+    div.className = 'row';
+
+    // Create the inner content
+    div.innerHTML = `
+        <div schemeCode="${schemeCode}" schemeName="${schemeName}">
+            ${schemeName}
+            <button class="remove-button">x</button>
+        `;
+
+    // Add a click event listener to the button to remove the div
+    div.querySelector('.remove-button').addEventListener('click', function () {
+        div.parentNode.removeChild(div);
+        main()
+    });
+
+    return div;
+}
+
+function addMutualFundToSelectedList(ui) {
+    var selectedMutualFundsDiv = document.getElementById("selectedMutualFunds");
+    var schemeCode = ui.item.schemeCode;
+    var schemeName = ui.item.schemeName;
+
+    // Create the div with the close button and add it to the selectedMutualFundsDiv
+    var mutualFundDiv = createMutualFundDiv(schemeCode, schemeName);
+    selectedMutualFundsDiv.appendChild(mutualFundDiv);
+}
+
+// Usage in your select function
+// select: function (event, ui) {
+//     addMutualFundToSelectedList(ui);
+//     // Rest of your select function
+// }
---
+++ assets/js/logic.js
@@ -0,0 +1,189 @@
+function calcXirr(data, years) {
+    function parseDate(dateStr) {
+        const parts = dateStr.split('-');
+        return new Date(parts[2], parts[1] - 1, parts[0]);
+    }
+
+    // console.log(data)
+    for (const entry of data) {
+        entry.date = parseDate(entry.date);
+    }
+
+    data.sort((a, b) => a.date - b.date);
+
+    // console.log(data);
+
+    // Function to calculate the next date
+    function getNextDate(currentDate) {
+        const nextDate = new Date(currentDate);
+        nextDate.setDate(currentDate.getDate() + 1);
+        return nextDate;
+    }
+
+    missingData = []
+
+    // Fill missing dates with NAV values from the next available date
+    for (let i = 0; i < data.length - 1; i++) {
+        const currentDate = data[i].date;
+        const nextDate = data[i + 1].date;
+        const daysDiff = (nextDate - currentDate) / (1000 * 60 * 60 * 24);
+
+        if (daysDiff > 1) {
+            for (let j = getNextDate(currentDate); j < nextDate; j.setDate(j.getDate() + 1)) {
+                // const missingDate = getNextDate(currentDate);
+                // Create a new entry for the missing date with NAV from the next available date
+                missingData.push({
+                    date: new Date(j),
+                    nav: data[i + 1].nav, // Set NAV to the next available date's NAV
+                });
+            }
+        }
+    }
+
+    // console.log(data);
+    // console.log(missingData);
+
+    fullData = data.concat(missingData);
+    fullData.sort((a, b) => a.date - b.date);
+    // console.log(fullData)
+
+    const dateToNavDictionary = {};
+
+    for (const entry of fullData) {
+        dateToNavDictionary[entry.date] = parseFloat(entry.nav);
+    }
+
+    // console.log(dateToNavDictionary)
+
+    function getnthPreviousMonthDate(currentDate, months) {
+        const date = new Date(currentDate);
+        date.setMonth(currentDate.getMonth() - months);
+        return date;
+    }
+
+    months = 12 * years;
+    navStartingDate = fullData[0].date;
+
+    finalGraph = []
+
+    for (i = fullData.length - 1; i >= 0; i--) {
+        currentDate = fullData[i].date
+        firstDateForSip = getnthPreviousMonthDate(currentDate, months - 1);
+        // console.log("firstDateForSip: ", firstDateForSip)
+        if (firstDateForSip < navStartingDate) break;
+
+        invDates = []
+        amounts = []
+        units = 0
+        for (j = months - 1; j >= 0; j--) {
+            invDate = getnthPreviousMonthDate(currentDate, j);
+            amount = 100;
+            // console.log("nav: ", invDate, dateToNavDictionary[invDate])
+            units = units + amount / dateToNavDictionary[invDate]
+
+            invDates.push(invDate);
+            amounts.push(amount * -1.0);
+        }
+
+        invDates.push(currentDate);
+        amounts.push(units * dateToNavDictionary[currentDate])
+
+        xirr = XIRR(amounts, invDates) * 100
+        // console.log(invDates);
+        // console.log(amounts);
+        // console.log("XIRR ----------------------: ", xirr)
+        finalGraph.push([currentDate.getTime(), xirr])
+
+    }
+    return finalGraph;
+}
+
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
+}
+
+function createChart(dataToPlot) {
+
+    var seriesData = dataToPlot.map(function (item) {
+        item.sipRollingReturnsData.sort((a, b) => a[0] - b[0]);
+        return {
+            name: item.schemeName,
+            data: item.sipRollingReturnsData,
+            tooltip: {
+                valueDecimals: 1
+            }
+        };
+    });
+
+    // console.log(seriesData)
+
+    // Create the chart
+    Highcharts.stockChart('container', {
+        rangeSelector: {
+            enabled: false
+        },
+        xAxis: {
+            type: 'datetime'
+        },
+        series: seriesData
+    });
+}
+
+async function main() {
+    var selectedMutualFundsDiv = document.getElementById("selectedMutualFunds");
+    var childDivs = selectedMutualFundsDiv.querySelectorAll("div.row > div");
+
+    years = getYearsFromRadioButton()
+
+    var dataToPlot = await Promise.all(Array.from(childDivs).map(async function (childDiv) {
+        var schemeCode = childDiv.getAttribute("schemecode");
+        var schemeName = childDiv.getAttribute("schemename");
+
+        const navData = await fetchData('https://api.mfapi.in/mf/' + schemeCode);
+        xirrData = calcXirr(navData.data, years)
+
+        // console.log("xirr:")
+        // console.log(xirrData)
+
+        return {
+            schemeName: schemeName,
+            sipRollingReturnsData: xirrData
+        };
+    }));
+
+    createChart(dataToPlot);
+}
+
+function getYearsFromRadioButton() {
+    if(document.getElementById('radioButton1').checked) {
+        return 1;
+    } else if(document.getElementById('radioButton2').checked) {
+        return 3;
+    } else if(document.getElementById('radioButton3').checked) {
+        return 5;
+    } else if(document.getElementById('radioButton4').checked) {
+        return 10;
+    } else {
+        return 1;
+    }
+}
+
+document.getElementById('radioButton1').addEventListener("click", function () {
+    main()
+});
+document.getElementById('radioButton2').addEventListener("click", function () {
+    main()
+});
+document.getElementById('radioButton3').addEventListener("click", function () {
+    main()
+});
+document.getElementById('radioButton4').addEventListener("click", function () {
+    main()
+});
